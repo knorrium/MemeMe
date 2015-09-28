a69a862d7664ea6a8b51f623f887f1ec541e9fd2
Felipe Knorr Kuhn <git@knorrium.info>
2015-09-27 21:09:13 -0700

    add the Meme model

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Felipe Kuhn on 9/27/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    // structure for storing memes
+    var topText = ""
+    var bottomText = ""
+    var image: UIImage
+    var memedImage: UIImage
+}
