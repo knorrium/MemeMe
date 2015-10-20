2862144351ecf0ecf5b34e94fa0f17a8d387b000
Felipe Knorr Kuhn <git@knorrium.info>
2015-10-19 20:52:24 -0700

    implement the collection view controller

---
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Felipe Kuhn on 10/14/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesCollectionViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    let cellReuseId = "SentMemesCollectionViewCell"
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    var memes: [Meme] {
+        return appDelegate.memes
+    }
+    
+    override func viewDidLoad() {
+        
+        let space: CGFloat = 3.0
+        let dimension = (self.view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+        
+        collectionView.backgroundColor = UIColor.blackColor()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.collectionView!.reloadData()
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("SentMemesCollectionViewCell", forIndexPath: indexPath) as! SentMemesCollectionViewCell
+        
+        let meme = self.appDelegate.memes[indexPath.row]
+        
+        cell.memedImageView.image = meme.memedImage
+
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return appDelegate.memes.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+
+        let detailController = self.storyboard!.instantiateViewControllerWithIdentifier("SentMemesDetailViewController") as! SentMemesDetailViewController
+        
+        detailController.memedImage = appDelegate.memes[indexPath.row].memedImage
+        
+        navigationController!.pushViewController(detailController, animated: true)
+    }
+
+}
