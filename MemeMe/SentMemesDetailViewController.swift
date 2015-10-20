85b5cdeb788eb402bebc677aff904d1d7b2d15d4
Felipe Knorr Kuhn <git@knorrium.info>
2015-10-19 22:20:38 -0700

    add the edit button on the view details VC

--- MemeMe/SentMemesDetailViewController.swift
+++ MemeMe/SentMemesDetailViewController.swift
@@ -11,13 +11,31 @@ import UIKit
 
 class SentMemesDetailViewController: UIViewController {
     var memedImage: UIImage!
+    var savedIndex: Int!
     
     @IBOutlet var imageView: UIImageView!
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: Selector("editMeme"))
         imageView.image = memedImage
+        tabBarController?.tabBar.hidden = false
+    }
+    
+    func editMeme() {
+        if navigationItem.rightBarButtonItem!.title == "Edit" {
+            navigationItem.rightBarButtonItem!.title = "Done"
+            let editController = storyboard!.instantiateViewControllerWithIdentifier("EditMemeViewcontroller") as! ViewController
+            editController.savedIndex = savedIndex
+
+            tabBarController?.tabBar.hidden = true
+            navigationController!.pushViewController(editController, animated: true)
+        }
+        else {
+            navigationItem.rightBarButtonItem!.title = "Edit"
+            tabBarController?.tabBar.hidden = false
+        }
+
     }
     
 }
