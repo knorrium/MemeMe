8a41c7ea6471c9c73f417b148a0afd51cb1869e8
Felipe Knorr Kuhn <git@knorrium.info>
2015-10-19 20:52:36 -0700

    implement the table view controller

---
+++ MemeMe/SentMemesTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Felipe Kuhn on 10/16/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        tableView.registerClass(SentMemesTableViewCell.self, forCellReuseIdentifier: "sentMemesTableViewCell")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.appDelegate.memes.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        NSLog("tableView");
+        let cell = tableView.dequeueReusableCellWithIdentifier("sentMemesTableViewCell")!
+        let meme = self.appDelegate.memes[indexPath.row]
+        
+        cell.textLabel?.text = meme.topText + " " + meme.bottomText
+        cell.imageView?.image = meme.memedImage
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let detailController = storyboard!.instantiateViewControllerWithIdentifier("SentMemesDetailViewController") as! SentMemesDetailViewController
+
+        detailController.memedImage = appDelegate.memes[indexPath.row].memedImage
+
+        self.navigationController!.pushViewController(detailController, animated: true)
+    }
+}
