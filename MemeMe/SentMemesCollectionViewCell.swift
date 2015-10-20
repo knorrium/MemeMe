6bff9ecbd41402bc3dea103d621e82876ce37043
Felipe Knorr Kuhn <git@knorrium.info>
2015-10-19 20:51:40 -0700

    add the custom ViewCell

---
+++ MemeMe/SentMemesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Felipe Kuhn on 10/14/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memedImageView: UIImageView!
+    
+}
