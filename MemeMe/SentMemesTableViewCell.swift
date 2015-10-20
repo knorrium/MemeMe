ea12fcafc84586e3a35ac4dc21f5929368701c2b
Felipe Knorr Kuhn <git@knorrium.info>
2015-10-19 20:54:14 -0700

    add the placeholder custom table view cell

---
+++ MemeMe/SentMemesTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemesTableViewCell.swift
+//  MemeMe
+//
+//  Created by Felipe Kuhn on 10/16/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class SentMemesTableViewCell: UITableViewCell {    
+//    @IBOutlet weak var memedImageView: UIImageView!
+}
